0bb963eab838eaa5831511f5dc6f4bfd34145311
Prince Mishra <84147697+curiosityengine@users.noreply.github.com>
2026-04-15 17:50:17 +0530

    Update data.js

--- projects/wallverse/data.js
+++ projects/wallverse/data.js
@@ -2,21 +2,41 @@ const wallpapers = [
   {
     url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e",
     category: "nature",
-    tags: ["ocean", "sea", "sunset", "water"]
+    tags: ["ocean"]
   },
   {
-    url: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+    url: "https://images.unsplash.com/photo-1491553895911-0055eca6402d",
+    category: "city",
+    tags: ["buildings"]
+  },
+  {
+    url: "https://images.unsplash.com/photo-1500534314209-a25ddb2bd429",
     category: "nature",
-    tags: ["forest", "trees", "green"]
+    tags: ["mountains"]
+  },
+  {
+    url: "https://images.unsplash.com/photo-1519681393784-d120267933ba",
+    category: "anime",
+    tags: ["art"]
+  },
+  {
+    url: "https://images.unsplash.com/photo-1501785888041-af3ef285b470",
+    category: "nature",
+    tags: ["forest"]
+  },
+  {
+    url: "https://images.unsplash.com/photo-1470770841072-f978cf4d019e",
+    category: "nature",
+    tags: ["sunset"]
   },
   {
     url: "https://images.unsplash.com/photo-1494526585095-c41746248156",
     category: "city",
-    tags: ["traffic", "night", "lights"]
+    tags: ["traffic"]
   },
   {
-    url: "https://images.unsplash.com/photo-1519125323398-675f0ddb6308",
-    category: "anime",
-    tags: ["illustration", "art"]
+    url: "https://images.unsplash.com/photo-1492724441997-5dc865305da7",
+    category: "city",
+    tags: ["night"]
   }
 ];
